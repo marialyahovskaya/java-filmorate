0a785f1ccc6c33b93e72da3ae4462d8e234dd357
Maria Lyahovskaya <marialyahovskaya@yandex.ru>
2022-09-10 22:15:47 +0300

    Сделан функционал

---
+++ src/main/java/ru/yandex/practicum/filmorate/Controller/ValidationException.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.filmorate.Controller;
+
+public class ValidationException extends Exception{
+    public ValidationException(String message) {
+        super(message);
+    }
+}
--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,4 +1,4 @@
-package test.java.ru.yandex.practicum.filmorate;
+package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
